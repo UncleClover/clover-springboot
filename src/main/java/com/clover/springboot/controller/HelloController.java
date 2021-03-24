0495de2679712c6c896fe0fd103b0e18b9252e93
UncleClover <qiang900714@126.com>
2021-03-24 19:56:16 +0800

    ceshi

--- src/main/java/com/clover/springboot/controller/HelloController.java
+++ src/main/java/com/clover/springboot/controller/HelloController.java
@@ -1,6 +1,6 @@
 package com.clover.springboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/hello")
+	@RequestMapping("/hello")
 	public String index() {
 		System.out.println("==================测试方法=====================");
 		return "helloworld";
